0330b9fd5528ac8c84528e18dfcf1e6aa6204991
OksanaTarun <oxana.tarun@gmail.com>
2022-11-01 17:46:10 +0300

    errCorr

---
+++ hw-js-assignments/calculations.js
@@ -0,0 +1,26 @@
+function increaseBalance(x, y) {
+  x = x + y;
+  return x;
+}
+
+//не передана переменная y
+function decreaseBalance(x, y) {
+  x = x - y;
+  return x;
+}
+
+//присутствует недостижимы код, вызываем ниже
+function divideBalanceByAccounts(x, y) {
+  x = x / y;
+  return x;
+}
+
+function getRestAfterDivision(x, y) {
+  x = x % y;
+  return x;
+}
+
+console.log(increaseBalance(3000, 700));
+console.log(decreaseBalance(3000, 700));
+console.log(divideBalanceByAccounts(3000, 2)); // лишнее значение 4000, передали только 2 параметра x и y
+console.log(getRestAfterDivision(7000, 3));
